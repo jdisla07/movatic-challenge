b181769e04e35d3a83a4afe4cbd30c36c2e3c0e3
jdisla07 <71913937+jdisla07@users.noreply.github.com>
2022-01-10 18:56:12 -0400

    getting station by id

--- server/server.py
+++ server/server.py
@@ -10,5 +10,13 @@ def get_stations_by_system(system):
     return response.json()['data']
 
 
+@app.route('/station/<system>/<station_id>', methods=['GET'])
+def get_station_by_id(system, station_id):
+    status = requests.get(f'https://gbfs.bcycle.com/{system}/station_status.json')
+    json_status = status.json()['data']['stations']
+    result_status = list(filter(lambda x: (x["station_id"] == station_id), json_status))
+    return result_status[0]
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
